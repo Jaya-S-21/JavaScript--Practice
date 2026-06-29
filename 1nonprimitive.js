b50af0a203c678ac2a819bd829bf2fb9113a929e
Jaya <jayasankhla50020712@gmail.com>
2026-06-29 23:34:21 +0530

    datatypes practice

--- 1nonprimitive.js
+++ 1nonprimitive.js
@@ -1,2 +1,32 @@
 //non- primitive data type
 //array, object, function
+
+//array
+let arr = [10,20,30,"Jaya",true]
+console.log(arr);
+console.log(typeof arr);       //object - not bug
+
+//object
+let obj = {
+    name:"Jaya",
+    account:"123120",
+    age:25,
+    category:"gen"
+}
+console.log(obj);
+console.log(typeof obj);
+
+//function
+function hello() {
+    console.log("Hello Everyone!!!");
+}
+hello();
+//console.log(typeof hello());
+
+//can be called in variables
+let f = function name() {
+    console.log("Jaya")
+}
+console.log(f);
+f();
+console.log(typeof f);           //function but actually object
--- 1primitive.js
+++ 1primitive.js
@@ -5,32 +5,39 @@
 let a = 10;
 let b = 55.23;
 console.log(a, b);
+console.log(typeof a);
 
 //string
 let c = "Hello Coder";
 let d = 'Jaya';
 console.log(c, d);
+console.log(typeof d);
 
 //boolean
 let login = true;
 let f = false;
 console.log(login, f);
+console.log(typeof f);
 
 //undefined
 let user;
 //const u;       cannot be undefined  
 console.log(user);
+console.log(typeof user);
 
 //bignit
 let g = 4652456345631254n;
 console.log(g);
+console.log(typeof g);
 
 //null
 let h = null;
 console.log(h);
+console.log(typeof h);    //object - bug
 
 //symbol
 const id1 = Symbol("id");
 const id2 = Symbol("id");
 console.log(id1, id2);
 console.log(id1 == id2);
+console.log(typeof id1);
