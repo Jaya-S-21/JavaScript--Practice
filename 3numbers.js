8b6202ab20f1c69ba20fc52d94424a6733087bc5
Jaya <jayasankhla50020712@gmail.com>
2026-07-04 23:51:18 +0530

    numbers

---
+++ 3numbers.js
@@ -0,0 +1,13 @@
+//number
+let a = 10;
+let b = 3.145672;
+//let c = b.toFixed(4)
+//console.log(typeof c);
+//console.log(typeof b.toPrecision(4));
+//console.log(b.toString());
+
+// let c = new Number(20);
+// let d = new Number(20);
+// console.log(c == d);                                    //false
+// //console.log(typeof c);
+// console.log(Boolean(new Number(0)));                    //false
